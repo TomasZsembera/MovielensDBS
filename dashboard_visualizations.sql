ba283a4ed0918a42a47aeeb55a9c8dd8cb96e113
TomasZsembera <139470039+TomasZsembera@users.noreply.github.com>
2025-01-10 23:56:31 +0100

    edited visualizations

--- dashboard_visualizations.sql
+++ dashboard_visualizations.sql
@@ -8,17 +8,14 @@ GROUP BY m.title
 ORDER BY total_ratings DESC
 LIMIT 10;
 
--- 2. Počet vydaných filmov pre každý žáner v roku 2000
+-- 2. Najčastejšie používané tagy (TOP 10)
 SELECT 
-    g.name AS genre,
-    COUNT(m.dim_moviesId) AS movie_count
-FROM dim_movies m
-JOIN genres_movies_staging gm ON m.dim_moviesId = gm.movie_id
-JOIN genres_staging g ON gm.genre_id = g.id
-WHERE m.release_year = '2000'
-GROUP BY g.name
-ORDER BY movie_count DESC;
-
+    t.tag AS tag, 
+    COUNT(t.dim_tagid) AS num_uses
+FROM dim_tags t
+GROUP BY t.tag
+ORDER BY num_uses DESC
+LIMIT 10;
 -- 3. Trend priemerného hodnotenia filmov podľa roku vydania
 SELECT 
     m.release_year AS year,
@@ -29,11 +26,17 @@ WHERE m.release_year IS NOT NULL
 GROUP BY m.release_year
 ORDER BY m.release_year;
 
--- 4. Počet filmov podľa žánru
-SELECT genre, COUNT(*) AS movie_count
-FROM dim_movies
-GROUP BY genre
-ORDER BY movie_count DESC;
+-- 4. TOP 10 filmov podľa priemerného hodnotenia
+SELECT 
+    m.title AS movie_title, 
+    ROUND(AVG(r.rating), 2) AS average_rating, 
+    COUNT(r.rating) AS num_ratings
+FROM fact_rating r
+JOIN dim_movies m ON r.dim_movieid = m.dim_movieid
+GROUP BY m.title
+HAVING COUNT(r.rating) >= 50
+ORDER BY average_rating DESC
+LIMIT 10;
 
 -- 5. Počet filmov podľa roku vydania
 SELECT release_year, COUNT(*) AS movie_count
